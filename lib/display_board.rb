b11ed2b8031a92856b63d43fb14a51da4924d6b3
vincent servio <vincentservio@gmail.com>
2019-08-25 01:54:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,10 @@ board = [" "," "," "," "," X"," "," "," "," ",]
 
 
 def display_board (board)
-  puts row =  "  #{board[0]}  |  #{board[1]}  |   #{board[2]} "
+  puts row = " #{board[0]} | #{board[1]} | #{board[2]} "
   puts separator = "-----------"
     puts row =  "  #{board[3]}  |  #{board[4]}  |  #{board[5]}  "
   puts separator = "-----------"
-  puts row =  "  #{board[6]}  |  #{board[7]}  |   #{board[8]} "
+  puts row =  " #{board[6]} | #{board[7]} | #{board[8]} ""
 
 end
